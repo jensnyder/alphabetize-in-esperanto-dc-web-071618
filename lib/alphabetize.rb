6ea2ab5d7d6bb863de3b855dddf10f39ab6a02e0
Jennifer Snyder <jlsnyder09@gmail.com>
2018-07-02 13:54:25 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -12,6 +12,3 @@ end
 alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 expressions = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
 puts alphabetize(expressions)
-puts expressions[0][0]
-puts alphabet.index("m")
-puts alphabet.index(expressions[0][0])
