08496f01da57ca492c6680e4a5e782cdd3e023b1
Jennifer Snyder <jlsnyder09@gmail.com>
2018-07-02 13:08:53 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,5 @@
-ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-
 def alphabetize(arr)
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |a,b|
     ESPERANTO_ALPHABET.index(b) <=> ESPERANTO_ALPHABET.index(a)
   end
