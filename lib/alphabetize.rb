2be8bddba53359dab2b0f13b40e758b04d585104
Jennifer Snyder <jlsnyder09@gmail.com>
2018-07-02 13:14:28 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,8 @@
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  puts alphabet.index("#{arr[1]}")
+  puts alphabet.index("#{arr[1][0]}")
 end
 
 expressions = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
 puts alphabetize(expressions)
+puts alphabet.index("k")
